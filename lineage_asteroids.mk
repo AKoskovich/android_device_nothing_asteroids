c4b49feb7308e24fbdb908f8fdc134b6bd533770
Alexander Koskovich <akoskovich@pm.me>
2025-06-13 09:44:42 -0400

    asteroids: Update to V3.1-250529-1004
    Change-Id: I8ef37d73fe21b63fd505cfdc76526053580902e1

--- lineage_asteroids.mk
+++ lineage_asteroids.mk
@@ -18,8 +18,8 @@ PRODUCT_NAME := lineage_asteroids
 PRODUCT_GMS_CLIENTID_BASE := android-nothing
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi_64-user 15 AQ3A.241015.001 2504171222 release-keys" \
-    BuildFingerprint=Nothing/Asteroids/Asteroids:15/AQ3A.241015.001/2504171222:user/release-keys \
+    BuildDesc="qssi_64-user 15 AQ3A.241015.001 2505291004 release-keys" \
+    BuildFingerprint=Nothing/Asteroids/Asteroids:15/AQ3A.241015.001/2505291004:user/release-keys \
     DeviceName=Asteroids \
     DeviceProduct=Asteroids \
     SystemDevice=Asteroids \
