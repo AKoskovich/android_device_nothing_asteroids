1e823618c9b88fa9aaecd95d2f57011159093221
Alexander Koskovich <akoskovich@pm.me>
2025-06-23 22:06:21 -0400

    asteroids: Cleanup NFC config & firmware handling a bit
    Also make sure that we're loading the ST54J specific firmware
    for the Japan model.
    
    Change-Id: I2114a0d98f36c7fa8ce8948029dbcc076e452525

--- device.mk
+++ device.mk
@@ -556,6 +556,13 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-hal-st-JPN.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-hal-st-JPN.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-hal-st-PRO.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-hal-st-PRO.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-hal-st-BASE.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-hal-st-BASE.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci-JPN.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci-JPN.conf
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml
